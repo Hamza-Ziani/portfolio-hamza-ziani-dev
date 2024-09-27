d2047bfec3a84ab18a5f707ef46a8358ff14ba11
Hamza-Ziani <hamzaziani381@gmail.com>
2024-09-27 14:50:26 +0100

    'final code source'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FormationsComponent } from './components/formations/formations.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',component : HomeComponent
+  },
+  {
+    path: 'about',component : AboutComponent
+  },
+  {
+    path: 'contact',component : ContactComponent
+  },
+  {
+    path: 'formations',component : FormationsComponent
+  },
+  {
+    path: 'projects',component : ProjectsComponent
+  },
+  {
+    path: 'skills',component : SkillsComponent
+  },
+  {
+    path: 'services',component : ServicesComponent
+  }
+];
